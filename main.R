740de7eeb12b6139bb3bf3e20adb5a5eb65884a7
Gerrit Konrad <konrad.gerritniklas@fh-swf.de>
2024-05-24 02:34:30 +0200

    Check whether Word is in row, column, reverse row, reverse column, if so then sample the corresponding new one

--- main.R
+++ main.R
@@ -1,18 +1,53 @@
 
-evilPuzzel <- function(word = 'mein', nrow = 10, ncolumn = 10, includeWord = TRUE){
+evilPuzzel <- function(word = 'mein', nrow = 10, ncol = 10, includeWord = TRUE){
   # Convert word in vector
   letters <- strsplit(word, split = "")[[1]] # Returns a list
   
-  sampleSize <- nrow * ncolumn
+  sampleSize <- nrow * ncol
   
   matrix <- matrix(sample(letters, size = sampleSize, replace = TRUE), 
                    nrow=nrow, 
-                   ncol=ncolumn
+                   ncol=ncol
             )
+  
+  # Check rows 
+  for (i in 1:nrow) {
+    if (findWordInVector(letters, matrix[i, ])) {
+      # Resample the row
+      matrix[i, ] <- sample(letters, ncol, replace = TRUE)
+    }
+  }
+  
+  # Check columns
+  for (i in 1:ncol) {
+    if (findWordInVector(letters, matrix[, i])) {
+      # Resample the column
+      matrix[i, ] <- sample(letters, nrow, replace = TRUE)
+    }
+  }
+  
+  # Check reverse rows
+  for (i in 1:nrow) {
+    if (findWordInVector(letters, rev(matrix[i, ]))) {
+      # Resample the row
+      matrix[i, ] <- sample(letters, ncol, replace = TRUE)
+    }
+  }
+  
+  # Check reverse columns
+  for (i in 1:ncol) {
+    if (findWordInVector(letters, rev(matrix[, i]))) {
+      # Resample the column
+      matrix[i, ] <- sample(letters, nrow, replace = TRUE)
+    }
+  }
+  
+  # Check diagonal 
+  
+  # Check reverse diagonal
+  
+  matrix
 }
-
-evilPuzzel()
-
 
 findWordInVector <- function(word, vector) {
   j <- 1
@@ -31,6 +66,8 @@ findWordInVector <- function(word, vector) {
   return(FALSE)
 }
 
+
+evilPuzzel()
 
 
 
